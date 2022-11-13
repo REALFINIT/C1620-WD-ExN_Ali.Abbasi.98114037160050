5f41c33218cb9d5602c6c1cafbb3b51f39246944
REALFINIT <53615544+REALFINIT@users.noreply.github.com>
2022-11-14 01:51:29 +0330

    import from Ex2

---
+++ scripts.js
@@ -0,0 +1,6 @@
+alert("welcome");
+var personname = prompt("what's your name?");
+if (personname != null) {
+    document.getElementById("name").innerHTML =
+    "Hi " + personname + "! wellcome to my site:)";
+  }
