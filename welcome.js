3a8e7f2077ac21699ab29715d8c3a4531210127c
REALFINIT <53615544+REALFINIT@users.noreply.github.com>
2022-11-30 17:10:42 +0330

    rename of first shown js

---
+++ welcome.js
@@ -0,0 +1,12 @@
+alert("welcome");
+var personname = prompt("what's your name?");
+    if (personname != null) {
+    document.getElementById("name").innerHTML =
+    "Hi " + personname + "! wellcome to my site:)";
+  }
+function themefunc() {
+    if (themefunc != null) {
+      document.getElementById("themeask").innerHTML =
+      "which color do you perfer?";
+    }
+  }
